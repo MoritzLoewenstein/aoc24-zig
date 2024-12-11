3681fcb4955a192897b10c9d3a6a3e2f645269f4
Moritz Löwenstein <moritz.loewenstein@gmx.de>
2024-12-11 23:52:40 +0100

    optimize 09 task 2

--- src/09/solution_2.zig
+++ src/09/solution_2.zig
@@ -18,109 +18,58 @@ test "result full" {
     try std.testing.expect(try defragment_filesystem_block_checksum(allocator, input_full) == 6360363199987);
 }
 
-const File = struct { id: u64, len: u64, idx: u64, checksum: u64 };
-const Empty = struct { len: u64, idx: u64 };
-const Field = enum { file, empty };
-const TaggedField = union(Field) { file: File, empty: Empty };
+const File = struct { id: u64, idx: u64, len: u4 };
+const Empty = struct { idx: u64, len: u4 };
 pub fn defragment_filesystem_block_checksum(allocator: std.mem.Allocator, input: []const u8) !u64 {
     const line_end_idx = std.mem.indexOfScalar(u8, input, '\n') orelse 0;
     if (line_end_idx == 0) return 0;
-    var list = std.ArrayList(TaggedField).init(allocator);
-    defer list.deinit();
+    var file_list = std.ArrayList(File).init(allocator);
+    defer file_list.deinit();
+    var empty_list = std.ArrayList(Empty).init(allocator);
+    defer empty_list.deinit();
     var fs_idx: u64 = 0;
+    var is_file: bool = true;
     for (0..line_end_idx) |idx| {
-        const is_file_len = idx % 2 == 0;
-        const len: u64 = input[idx] - '0';
-        if (is_file_len) {
-            const file_id: u64 = idx / 2;
-            const checksum = file_get_checksum(file_id, len, fs_idx);
-            try list.append(.{ .file = .{ .id = file_id, .len = len, .idx = fs_idx, .checksum = checksum } });
+        const len: u4 = try std.fmt.parseInt(u4, &[_]u8{input[idx]}, 10);
+        if (is_file) {
+            try file_list.append(.{
+                .idx = fs_idx,
+                .id = idx / 2,
+                .len = len,
+            });
         } else {
-            try list.append(.{ .empty = .{ .idx = fs_idx, .len = len } });
+            try empty_list.append(.{ .idx = fs_idx, .len = len });
         }
         fs_idx += len;
+        is_file = !is_file;
     }
 
-    if (list.items.len == 0) {
+    if (empty_list.items.len == 0) {
         std.debug.print("no defragmentation necessary, no empty places\n", .{});
         return 0;
     }
 
-    var file_idx: u64 = list.items.len - 1;
-    var empty_idx: u64 = 1;
-    while (true) {
-        if (file_idx == 0) {
-            break;
-        }
-        if (empty_idx > file_idx) {
-            empty_idx = 1;
-            file_idx -= 2;
-            continue;
-        }
-
-        const file = list.items[file_idx].file;
-        const empty = list.items[empty_idx].empty;
-        if (empty.len < file.len) {
-            empty_idx += 2;
-            continue;
-        }
-
-        list.items[file_idx].file.checksum = file_get_checksum(file.id, file.len, empty.idx);
-        list.items[file_idx].file.idx = empty.idx;
-
-        list.items[empty_idx].empty.idx += file.len;
-        list.items[empty_idx].empty.len -= file.len;
-        empty_idx = 1;
-        file_idx -= 2;
-    }
-
+    var file_idx: u64 = file_list.items.len - 1;
     var fs_checksum: u64 = 0;
-    for (list.items) |item| {
-        switch (item) {
-            .file => |*file| {
-                fs_checksum += file.*.checksum;
-            },
-            else => {},
+    file_loop: while (file_idx > 0) : (file_idx -= 1) {
+        const file = file_list.items[file_idx];
+        for (0..file_idx) |empty_idx| {
+            const empty = empty_list.items[empty_idx];
+            if (empty.len >= file.len) {
+                fs_checksum += file_get_checksum(file.id, empty.idx, file.len);
+                empty_list.items[empty_idx].idx += file.len;
+                empty_list.items[empty_idx].len -= file.len;
+                continue :file_loop;
+            }
         }
+        fs_checksum += file_get_checksum(file.id, file.idx, file.len);
     }
-    //try print_tagged_fields(allocator, list.items);
+
     return fs_checksum;
 }
 
-pub fn file_get_checksum(id: u64, len: u64, idx: u64) u64 {
-    var checksum: u64 = 0;
-    for (idx..idx + len) |mul| {
-        checksum += id * mul;
-    }
-    return checksum;
-}
-
-pub fn print_tagged_fields(allocator: std.mem.Allocator, list: []const TaggedField) !void {
-    var files = std.ArrayList(File).init(allocator);
-    defer files.deinit();
-    for (list) |item| {
-        switch (item) {
-            .file => |*file| {
-                try files.append(file.*);
-            },
-            else => {},
-        }
-    }
-    std.mem.sort(File, files.items, {}, file_compare_idx_asc);
-
-    var prev_end_idx: u64 = 0;
-    for (files.items) |file| {
-        for (prev_end_idx..file.idx) |idx| {
-            std.debug.print("{any} -> empty\n", .{idx});
-        }
-        for (file.idx..file.idx + file.len) |idx| {
-            std.debug.print("{any} -> {any}\n", .{ idx, file.id });
-        }
-        prev_end_idx = file.idx + file.len;
-    }
-}
-
-pub fn file_compare_idx_asc(context: void, a: File, b: File) bool {
-    _ = context;
-    return a.idx < b.idx;
+pub fn file_get_checksum(id: u64, idx: u64, len: u4) u64 {
+    // use arithmetic progression formula of range idx to idx + len
+    // multiply with file id to get checksum of file
+    return id * (((idx + idx + len - 1) * len) / 2);
 }
